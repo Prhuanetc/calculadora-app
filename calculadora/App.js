d7236c02b03ff8745ea38e60430c95eca8ae7fa1
Prhuanetc <email.rhuan.etec@gmail.com>
2026-02-19 11:41:19 -0300

    19/02

---
+++ calculadora/App.js
@@ -0,0 +1,27 @@
+import { useMemo, useState } from 'react';
+import { StatusBar, StyleSheet, Text, Pressable, View } from 'react-native';
+
+import CalcButton from './components/CalcButton';
+import Display from './components/Display';
+import { themes } from './theme/token';
+import { createEngine } from "./utils/calcEngine";
+
+export default function App() {
+  const [mode, setMode] = useState("dark");
+  const theme = themes[mode];
+  return (
+    <View style={[styles.container, {backgroundColor: theme.bg}]}>
+      <Text>aaaaaaaaaaaaaaaaaaaaaaaa</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ calculadora/theme/token.js
@@ -0,0 +1,31 @@
+export const themem = {
+    dark: {
+        mode: "dark",
+        bg: "#17181A",
+        card: "#202124",
+        stroke: "rgba(255,255,255,0.08)",
+        text: "#f2f2f7",
+        textDim: "rgba(242,242,242,0.55)",
+        op: "#F59e08",
+        opText: "#141414",
+        key: "#2a2b2f",
+        keyText: "#f2f2f7",
+        shadowDark: "rgba(0,0,0,0.55)",
+        shadowLight: "gba(255,255,255,0.06)",
+    },
+    light: {
+        mode: "light",
+        bg: "#EDEF2",
+        card: "#F67FB",
+        stroke: "rgba(17,24,39,0.08)",
+        text: "#111827",
+        textDim: "rgba(17,24,39,0.55)",
+        op: "#F59e08",
+        opText: "#141414",
+        key: "#F6F7FB",
+        keyText: "#111827",
+        shadowDark: "rgba(17,24,39,0.18)",
+        shadowLight: "rgba(255,255,255,0.05)",
+    }
+
+};
